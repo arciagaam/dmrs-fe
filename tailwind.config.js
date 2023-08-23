87032574e44f448cf75d4c9cd4a1ec2255b9a000
Vengelinee <vengelineangot@gmail.com>
2023-08-23 23:30:50 +0800

    Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,56 @@ export default {
   theme: {
     extend: {
       colors:{
-        testGreen : '#55D6BD'
+        testGreen : '#55D6BD',
+        primary: {
+          50: '#f1fcf9',
+          100: '#d1f6ed',
+          200: '#a3ecdb',
+          300: '#55d6bd', // default
+          400: '#3ec3ad',
+          500: '#25a794',
+          600: '#1b8679',
+          700: '#1a6b62',
+          800: '#195650',
+          900: '#194843',
+          950: '#082b29'
+        },
+
+        secondary: {
+          50: '#fcfcfc', // default light
+          100: '#efefef',
+          200: '#dcdcdc',
+          300: '#bdbdbd',
+          400: '#989898',
+          500: '#7c7c7c',
+          600: '#656565',
+          700: '#525252',
+          800: '#464646',
+          900: '#3d3d3d',
+          950: '#292929'
+        },
+        
+        accent: {
+          50: '#f6f4fe', 
+          100: '#eeeafd',
+          200: '#ded9fb',
+          300: '#c6baf8',
+          400: '#a992f3',
+          500: '#8860eb', // default 
+          600: '#7c46e1',
+          700: '#6d34cd',
+          800: '#5b2bac',
+          900: '#4c258d',
+          950: '#2f165f'
+        },
+
+        background: {
+          light: '#FFFFFF',
+          dark: '#100F13',
+        }
+
+
+
       },
       
       fontSize: {
