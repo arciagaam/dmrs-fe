14bb31d8464d6bbbcaa6d6e944c483002509b974
allenJcodes <110220645+allenJcodes@users.noreply.github.com>
2023-08-28 11:36:43 +0800

    dmrs thai

---
+++ src/pages/Home/thailand.jsx
@@ -0,0 +1,19 @@
+import Hero from "../../components/Hero"
+
+const Thailand = () => {
+  return (
+    <div className="flex flex-col w-full bg-white overflow-clip">
+			<Hero>
+				<div className="z-10 flex flex-col items-center justify-center w-full gap-3 text-white">
+					<div className="flex flex-col items-center justify-center">
+						<p className='z-10 text-xs'>DMRS</p>
+						<h1 className='z-10 text-lg text-center'>Dan Murdoch Risk Thailand</h1>
+					</div>
+				</div>
+			</Hero>
+    </div>
+    
+  )
+}
+
+export default Thailand
