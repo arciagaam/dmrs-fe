0e2874bead9dc83e1be3e0487bed96a5e3f641ef
allenJcodes <110220645+allenJcodes@users.noreply.github.com>
2023-09-01 00:38:34 +0800

    Updates: - bg marquee text fix

--- src/pages/Thailand/components/BgTextMarquee.jsx
+++ src/pages/Thailand/components/BgTextMarquee.jsx
@@ -1,16 +1,13 @@
 const BgTextMarquee = () => {
   const contents = ['INTELLIGENCE', 'RESEARCH', 'INVESTIGATION', 'OTHER SERVICES', 'INTELLIGENCE', 'RESEARCH']
-  const words = [];
-
-  contents.forEach((content, index) => {
-    words.push(<p className={"font-bold text-secondary-100/60 text-6xl z-0 whitespace-nowrap"} key={index}>{content}</p>)
-  })
 
   return (
     <div className="marquee flex flex-row gap-8 w-fit">
-      {words}
+      {contents.map((content, index) => (
+        <p className={"font-bold text-secondary-100/60 text-6xl z-0 whitespace-nowrap"} key={index}>{content}</p>
+      ))}
     </div>
   )
 }
-  
+
 export default BgTextMarquee
