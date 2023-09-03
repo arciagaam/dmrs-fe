f081e95460fb7c5f1b5853d2f61f2c400c5cf695
Vengelinee <vengelineangot@gmail.com>
2023-09-03 23:32:40 +0800

    update

--- src/pages/Australia/index.jsx
+++ src/pages/Australia/index.jsx
@@ -18,7 +18,7 @@ const Australia = () => {
       
 
       {/* DMRS Australia */}
-      <section className="relative navTrigger">
+      <section className="relative overflow-hidden navTrigger">
         <div className="flex items-center w-full gap-[6rem] p-[150px] tablet:flex-col">
           <div className="z-10 rounded-tl-[3.12rem] rounded-br-[3.12rem] flex w-1/2 h-full min-h-[450px] bg-no-repeat bg-center bg-cover bg-[url('assets/dmrs-australia-1.jpg')]">
 
@@ -56,7 +56,7 @@ const Australia = () => {
       </section>
 
       {/* Services */}
-      <section>
+      <section className=""> 
         <div className="flex flex-col h-[94vh]">
           <div className="flex flex-col w-full justify-center gap-5 px-[150px] py-[50px]">
             <p className="text-2xl font-bold text-center uppercase text-bg-dark ">Who are Dan Murdoch Risk Services?</p>
@@ -66,10 +66,17 @@ const Australia = () => {
               Pty Ltd Pty Ltd operates all services offered under the banners of DMRS, Dan Murdoch Risk Services, Your Intelligence Program, and DMRS VPN.
             </p>
           </div>
+          
+          <div className="flex w-full h-screen overflow-x-scroll">
+            <div className="flex flex-col items-center justify-center w-full flex-shrink-0 bg-no-repeat bg-bottom bg-cover bg-[url('assets/IntelligentIntelligence.png')]">
+              
+            </div>  
 
-          <div className="flex flex-col items-center justify-center w-full h-full p-[5rem] bg-no-repeat bg-bottom bg-cover bg-[url('assets/IntelligentIntelligence.png')]">
-            
+            <div className="flex flex-col items-center justify-center w-full flex-shrink-0 bg-no-repeat bg-bottom bg-cover bg-[url('assets/IntelligentIntelligence.png')]">
+              
+            </div>  
           </div>
+          
         </div>
       </section>
 
