0027e3ef4b02f00dd3fb9b86a5001831886de2f2
Vengelinee <vengelineangot@gmail.com>
2023-09-08 20:16:31 +0800

    Responsiveness update - not yet done

--- src/pages/Australia/index.jsx
+++ src/pages/Australia/index.jsx
@@ -20,26 +20,26 @@ const Australia = () => {
       
 
       {/* DMRS Australia */}
-      <section className="relative navTrigger">
-          <div className="flex items-center w-full min-h-[768px] h-screen gap-[6rem] p-20 tablet:flex-col">
-            <div className="z-10 flex w-1/2  max-h-[500px]">
-              <img src={Australia_img} alt="" className="object-cover cover rounded-tl-[3.12rem] rounded-br-[3.12rem]"/>
-            </div>
+      <section className="relative overflow-hidden navTrigger ">
+        <div className="flex items-center w-full min-h-[768px] h-screen gap-[6rem] p-20 tablet:flex-col">
+          <div className="z-10 flex w-1/2  max-h-[500px]">
+            <img src={Australia_img} alt="" className="object-cover cover rounded-tl-[3.12rem] rounded-br-[3.12rem]"/>
+          </div>
 
-            <div className="z-10 flex flex-col w-1/2 gap-10">
-              <div>
-                <p className="text-xl font-bold leading-[3rem] text-bg-dark">Dan Murdoch Risk Services</p>
-                <p className="text-2xl font-bold text-primary-300">Australia</p>
-              </div>
-              
-              <p className="z-10 max-w-lg text-small text-bg-dark">
-                <strong className="text-accent-800">DMRS </strong> 
-                <strong className="text-primary-400">Australia </strong> 
-                Pty Ltd is the parent company for the operational division Dan Murdoch Risk Services (Thailand) Co. Limited.
-              </p>
-            </div>  
+          <div className="z-10 flex flex-col w-1/2 gap-10">
+            <div>
+              <p className="text-xl font-bold leading-[3rem] text-bg-dark">Dan Murdoch Risk Services</p>
+              <p className="text-2xl font-bold text-primary-300">Australia</p>
+            </div>
+            
+            <p className="z-10 max-w-lg text-small text-bg-dark">
+              <strong className="text-accent-800">DMRS </strong> 
+              <strong className="text-primary-400">Australia </strong> 
+              Pty Ltd is the parent company for the operational division Dan Murdoch Risk Services (Thailand) Co. Limited.
+            </p>
           </div>  
-          <img src={DMRSimg} alt="" className="absolute top-0 bottom-0 right-[-10px] w-auto m-auto min-h-[600px] max-h-[850px] z-0 tablet:hidden"/>
+        </div>  
+        <img src={DMRSimg} alt="" className="absolute top-0 bottom-0 right-[-10px] w-auto m-auto min-h-[600px] max-h-[850px] z-0 tablet:hidden"/>
         
       </section>
 
@@ -58,25 +58,22 @@ const Australia = () => {
       </section>
 
       {/* Services */}
-      <section> 
-        <div className="flex flex-col h-[94vh]">
-          <div className="flex flex-col justify-center w-full gap-5 px-20 py-20">
-            <p className="text-2xl font-bold text-center uppercase text-bg-dark ">Who are Dan Murdoch Risk Services?</p>
-            <p className="self-center max-w-5xl min-w-[565px] text-center text-small text-bg-dark">
+      <section className=''> 
+        <div className="flex flex-col h-[95vh] laptop:min-w-[450px]">
+          <div className="flex flex-col justify-center w-full gap-5 px-20 pt-20 pb-10">
+            <p className="text-2xl font-bold text-center uppercase laptop:text-xl text-bg-dark ">Who are Dan Murdoch Risk Services?</p>
+            <p className="self-center max-w-5xl min-w-[565px] text-center text-small text-bg-dark laptop:text-xs">
               <strong className="text-accent-800">DMRS </strong> 
               <strong className="text-primary-400">Australia </strong> 
               Pty Ltd Pty Ltd operates all services offered under the banners of DMRS, Dan Murdoch Risk Services, Your Intelligence Program, and DMRS VPN.
             </p>
           </div>
           
-          <div className="flex w-full h-screen overflow-x-scroll">
-            <div className="flex flex-col items-center justify-center w-full flex-shrink-0 bg-no-repeat bg-bottom bg-cover bg-[url('assets/IntelligentIntelligence.png')]">
+          <div className="flex w-full h-full overflow-x-scroll">
+            <div className="flex flex-col items-center justify-center w-full bg-no-repeat bg-bottom bg-cover bg-[url('assets/IntelligentIntelligence.png')]">
               
             </div>  
 
-            <div className="flex flex-col items-center justify-center w-full flex-shrink-0 bg-no-repeat bg-bottom bg-cover bg-[url('assets/IntelligentIntelligence.png')]">
-              
-            </div>  
           </div>
           
         </div>
@@ -85,7 +82,7 @@ const Australia = () => {
       {/* Brochure */}
       <section>
         <div className="flex items-center w-full h-full">
-          <div className="flex flex-col w-auto h-full gap-10 py-20 pl-20 mr-5">
+          <div className="flex flex-col items-center w-auto h-full gap-10 py-20 pl-20 mr-5">
             <p className="text-2xl font-bold uppercase laptop:text-xl text-bg-dark ">Intelligence Investigations</p>
           </div>
 
@@ -94,8 +91,6 @@ const Australia = () => {
           </div>
         </div>  
       </section>
-
-
     </div>
   )
 }
