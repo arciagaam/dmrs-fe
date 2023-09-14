cbd2cc098a55de9f617376ffcda7ece0b15ee3c9
Vengelinee <vengelineangot@gmail.com>
2023-09-15 03:59:03 +0800

    responsiveness update

--- src/pages/Australia/index.jsx
+++ src/pages/Australia/index.jsx
@@ -9,7 +9,7 @@ import IntelligenceMb_img from '../../assets/intelligent-investigations-mb.png'
 const Australia = () => {
   return (
     <div className="flex flex-col w-full bg-white"> 
-      <div className="z-10">
+      <div className="tablet:z-10">
         <Hero>
           <div className="flex flex-col items-center justify-center w-full gap-3 text-white">
             <div className="flex flex-col items-center justify-center gap-4">
@@ -22,32 +22,32 @@ const Australia = () => {
       
 
       {/* DMRS Australia */}
-      <section className="relative overflow-hidden navTrigger ">
+      <section className="relative navTrigger tablet:z-10">
         <div className="flex flex-col h-full items-center w-full laptop:min-h-[768px] laptop:h-full gap-12 laptop:gap-[6rem] laptop:p-20 tablet:p-16 p-10 tablet:flex-row ">
-          <div className="max-h-[350px] z-10 flex w-full tablet:w-full  tablet:max-h-[500px]">
+          <div className="max-h-[350px] tablet:z-10 flex w-full tablet:w-full  tablet:max-h-[500px]">
             <img src={Australia_img} alt="" className="object-cover cover rounded-tl-[3.12rem] rounded-br-[3.12rem]"/>
           </div>
 
-          <div className="z-10 flex flex-col w-full gap-10 tablet:w-full">
+          <div className="flex flex-col w-full gap-10 tablet:z-10 tablet:w-full">
             <div>
               <p className="font-bold text-md laptop:text-xl text-bg-dark ">Dan Murdoch Risk Services</p>
               <p className="text-lg font-bold laptop:text-2xl text-primary-300">Australia</p>
             </div>
             
-            <p className="z-10 max-w-lg text-small text-bg-dark">
+            <p className="max-w-lg tablet:z-10 text-small text-bg-dark">
               <strong className="text-accent-800">DMRS </strong> 
               <strong className="text-primary-400">Australia </strong> 
               Pty Ltd is the parent company for the operational division Dan Murdoch Risk Services (Thailand) Co. Limited.
             </p>
           </div>  
         </div>  
-        <img src={DMRSimg} alt="" className="absolute top-0 bottom-0 right-[-10px] w-auto m-auto min-h-[600px] max-h-[850px] z-0 tablet:hidden"/>
+        <img src={DMRSimg} alt="" className="tablet:absolute tablet:top-0 tablet:bottom-0 tablet:right-0 tablet:w-auto tablet:m-auto tablet:min-h-[600px] tablet:max-h-[850px] tablet:z-0 tablet:block hidden"/>
         
       </section>
 
       {/* Intelligent Intelligence */}
-      <section>
-        <div className="flexh-full w-full items-center gap-[6rem] p-7 tablet:p-20">
+      <section className="relative tablet:z-10">
+        <div className="flex h-full w-full items-center gap-[6rem] p-7 tablet:p-20">
           <div className="flex flex-col tablet:flex-row tablet:p-10 laptop:p-20 justify-center items-center w-full max-h-[450px] p-10 gap-2 mobile:gap-5 rounded-[3.12rem] bg-black">
             <img src={II_icon} alt="" className="object-contain w-1/4"/>
             <div className="flex flex-col justify-center text-center tablet:text-left">
@@ -88,7 +88,7 @@ const Australia = () => {
       {/* Brochure */} 
       <section>
         <div className="flex flex-col-reverse items-center w-full tablet:min-h-[200px] h-full tablet:flex-row">
-          <div className="flex flex-col items-center w-auto h-full gap-5 p-10 mr-5 tablet:mr-0 laptop:p-20 laptop:pr-10 tablet:items-start">
+          <div className="flex flex-col items-center w-auto h-full gap-5 p-10 mr-5 tablet:mr-0 laptop:p-20 laptop:pr-5 tablet:items-start">
             <p className="font-bold text-center uppercase laptop:text-2xl text-md tablet:text-lg tablet:text-left text-bg-dark ">Intelligence Investigations</p>
             <Button variant="default" size="sm" >See Brochure</Button>
           </div>
