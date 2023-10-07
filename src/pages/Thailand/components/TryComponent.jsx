a525e139a0de9ff225467d7805e055cc59a0e422
allenJcodes <110220645+allenJcodes@users.noreply.github.com>
2023-10-07 21:43:51 +0800

    Responsiveness and animation

--- src/pages/Thailand/components/TryComponent.jsx
+++ src/pages/Thailand/components/TryComponent.jsx
@@ -2,27 +2,12 @@ import { useState, useEffect, useRef } from "react"
 import { easeInOut, motion, useDragControls} from 'framer-motion'
 
 const TryComponent = () => {
-  const [drag, setDrag] = useState(false);
-  const dragControls = useDragControls()
-  
-  function startDrag(event) {
-    dragControls.start(event, { snapToCursor: true })
-  }
-
   return (
     <motion.div 
-      drag={true}
-      onDragStart={() => setDrag(!drag)}
-      onDragEnd={() => setDrag(!drag)}
-      whileDrag={{ scale: 0.3, transition: { duration: 0.4}}}
-      sticky={true}
-      dragConstraints={{ right: 100 }}
-      dragSnapToOrigin={true}
-      initial={{ x: 0 }}
-      animate={{ opacity: [0,1], x: [-1680, 0], transition:{ duration: 1.5, ease: easeInOut}, mixBlendMode: "difference" }}
-      className="absolute flex items-center justify-center bg-white w-[400px] top-10 right-0 h-[400px] rounded-full z-20">
-      <p className={`text-black text-xs ${drag ? 'opacity-0' : 'opacity-100'}`}>Drag Me!</p>
-  </motion.div>
+      viewport={{once: true}}
+      whileInView={{scale: 40, transition:{ duration: 0.5, ease: easeInOut, repeat: false} }}
+      className="absolute flex items-center justify-center bg-background-dark w-[100px] h-[100px] left-[50%] rounded-full z-0">
+    </motion.div>
   )
 }
 
